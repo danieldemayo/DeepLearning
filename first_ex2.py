723c20f124f62c7ecca84140354bbbbb4da304e0
danield <demayodl@gmail.com>
2021-11-03 23:55:51 +0200

    part2

--- first_ex2.py
+++ first_ex2.py
@@ -14,11 +14,13 @@ Then, build your neural networks and find the architecture which gives you the b
 """
 
 import numpy as np
-from torch import nn
+from torch import nn, from_numpy, Tensor
 from torch.autograd import Variable
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, roc_curve, auc, roc_auc_score, RocCurveDisplay
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from torch.optim import SGD
+from sklearn.model_selection import train_test_split
 
 
 # **Generate data:**
@@ -43,21 +45,74 @@ def viz_data(x: np.ndarray, y: np.ndarray, z: np.ndarray):
     plt.show()
 
 
+def convert_to_tensor(array: np.ndarray):
+    return from_numpy(array.astype(np.float32))
+
+
 # **Define the Model:**
 
+
 class RegressionModel(nn.Module):
-    def __init__(self):
+    def __init__(self, num_inputs: int, num_neurons: list):
         super().__init__()
+        self.lin1 = nn.Linear(num_inputs, num_neurons[0])
+        self.lin2 = nn.Linear(num_neurons[0], num_neurons[1])
+        self.lin3 = nn.Linear(num_neurons[1], 1)
+
+        self.relu = nn.ReLU()
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.relu(self.lin1(x))
+        x = self.sig(self.lin2(x))
+        x = self.sig(self.lin3(x))
+        return x
 
 
 # **Training and validation:**
+model_conf = {
+    'loss_function': nn.MSELoss,
+    'optimizer': SGD,
+    'lr': 0.1,
+    'momentum': 0.9,
+    'num_of_epochs': 1000,
+}
 
-def train_model(model: nn.Module) -> nn.Module:
-    pass
+
+def train_model(model: nn.Module, conf: dict, x_train: Tensor, y_train: Tensor) -> tuple[nn.Module, list]:
+    losses = []
+    predictions = []
+    aucs = []
+    criterion = conf['loss_function']()
+    optimizer = conf['optimizer'](model.parameters(), lr=conf['lr'], momentum=conf['momentum'])
+
+    for epoch in range(conf['num_of_epochs']):
+        y_pred = model(x_train)
+        loss = criterion(y_pred, y_train)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        losses.append(loss.item())
+        predictions.append(y_pred.detach().numpy())
+        #todo: roc_auc_score
+        fpr, tpr, threshold = roc_curve(y_train, predictions[epoch])
+        aucs.append(auc(fpr, tpr))
+        if (epoch + 1) % 100 == 0:
+            print('epoch:', epoch + 1, ',loss=', loss.item())
+
+    scores = [losses, predictions, aucs]
+    return model, scores
 
 
-def validate_model(model: nn.Module) -> list:
-    pass
+def validate_model(model: nn.Module, x_test, y_test):
+    y_pred = model(x_test)
+    fpr, tpr, threshold = roc_curve(y_test.detach().numpy(), y_pred.detach().numpy())
+    roc_auc = auc(fpr, tpr)
+    print(roc_auc)
+    roc_curve_display = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc)
+    roc_curve_display.plot()
+    return roc_curve_display
 
 
 # **Visualizing the plots:**
