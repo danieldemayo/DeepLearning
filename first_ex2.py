64cd32581c387ed5cfbfab680249a7bba37ff46c
danield <demayodl@gmail.com>
2021-11-08 22:05:28 +0200

    .

--- first_ex2.py
+++ first_ex2.py
@@ -16,10 +16,7 @@ from typing import Tuple, List, Dict
 import numpy as np
 from numpy.typing import NDArray
 from torch import nn, from_numpy, Tensor, manual_seed, no_grad
-from torch.autograd import Variable
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import matplotlib.pyplot as plt
-from matplotlib import cm
 from torch.optim import SGD, Optimizer
 from sklearn.model_selection import train_test_split
 
@@ -39,27 +36,37 @@ def trigo_function(x1, x2):
     return np.sin(x1) * np.cos(x2) + 0.1 * np.random.rand(x1.shape[0], x1.shape[1])
 
 
-def generate_data() -> Tuple[NDArray, NDArray, NDArray]:
-    xx, yy = grid((-5, 5), (-5, 5))
-    z = trigo_function(xx, yy)
-    return xx, yy, z
+def generate_data() -> Tuple[Tuple, NDArray]:
+    plane = grid((-5, 5), (-5, 5))
+    fx = trigo_function(*plane)
+    return plane, fx
 
 
-def vectorize_data(x, y, z):
-    xx = x.ravel().reshape(-1, 1)
-    yy = y.ravel().reshape(-1, 1)
+def vectorize_data(x: NDArray, y: NDArray, z: NDArray) -> Tuple[NDArray, NDArray]:
+    xx = x.reshape(-1, 1)
+    yy = y.reshape(-1, 1)
     input_data = np.hstack((xx, yy))
     return input_data, z.reshape(-1, 1)
 
 
-def viz_data(x: NDArray, y: NDArray, z: NDArray):
+def viz_data(x: NDArray, y: NDArray, z: NDArray, show: bool = True):
     plt.figure()
     ax = plt.axes(projection='3d')
-    ax.contour3D(x, y, z, 50, cmap='binary')
+    ax.plot_surface(x, y, z, color='b', alpha=0.5)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
     ax.view_init(60, 35)
+    if show:
+        plt.show()
+    return ax
+
+
+def viz_preds(data, preds):
+    x, fx = vectorize_data(*data[0], data[1])
+    x_train, x_test, y_train, y_test = train_test_split(x, fx, test_size=0.3, random_state=1)
+    ax = viz_data(*data[0], data[1], show=False)
+    ax.scatter(x_test[:, 0], x_test[:, 1], preds, color='r')
     plt.show()
 
 
@@ -95,7 +102,7 @@ def train_model(
         optimizer: Optimizer,
         loss_fn: nn.MSELoss,
         x_train: Tensor, y_train: Tensor
-) -> float:
+) -> Tensor:
     model.train()
     optimizer.zero_grad()
     y_pred = model(x_train)
@@ -105,34 +112,36 @@ def train_model(
     return loss
 
 
-def test_model(model: nn.Module, x_test: Tensor, y_test: Tensor, loss_fn: nn.MSELoss) -> Tuple[float, Tensor]:
+def test_model(model: nn.Module, x_test: Tensor, y_test: Tensor, loss_fn: nn.MSELoss) -> Tuple[Tensor, Tensor]:
     model.eval()
     with no_grad():
         y_pred = model(x_test)
         loss = loss_fn(y_pred, y_test)
-    return loss, y_pred
+    return loss, y_pred.ravel()
 
 
-def run_model(model: nn.Module, data: Tensor, num_of_epochs: int) -> [List, List, List]:
+def run_model(model: nn.Module, data: Tuple[Tuple, NDArray], num_of_epochs: int) -> [List, List, Dict]:
     lr = 0.1
     momentum = 0.9
     train_losses = []
     test_losses = []
-    predictions = []
     loss_function = nn.MSELoss()
     optimizer = SGD(model.parameters(), lr=lr, momentum=momentum)
-    x_train, y_train, x_test, y_test = train_test_split(data, test_size=0.3)
+    x, fx = vectorize_data(*data[0], data[1])
+    x_train, x_test, y_train, y_test = (convert_to_tensor(v) for v in
+                                        train_test_split(x, fx, test_size=0.3, random_state=1))
+    pred = np.array([1])
     for epoch in range(num_of_epochs):
         train_loss = train_model(model, optimizer, loss_function, x_train, y_train)
         test_loss, test_pred = test_model(model, x_test, y_test, loss_function)
-        train_losses.append(train_loss)
-        test_losses.append(test_loss)
-        predictions.append(test_pred)
+        train_losses.append(train_loss.item())
+        test_losses.append(test_loss.item())
+        pred = test_pred.detach().numpy()
         if (epoch + 1) % 100 == 0:
-            print('epoch:', epoch + 1, ',train_loss=', train_loss)
-            print('epoch:', epoch + 1, ',test_loss=', test_loss)
+            print('epoch:', epoch + 1, ',train_loss =', train_loss.item())
+            print('epoch:', epoch + 1, ',test_loss =', test_loss.item())
         # validation usage
-    return train_losses, test_losses, predictions, dict(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+    return train_losses, test_losses, dict(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, pred=pred)
 
 
 # **Visualizing the plots:**
@@ -201,30 +210,28 @@ class OverfitModel(nn.Module):
 def main():
     manual_seed(1202)
     data = generate_data()
-    viz_data(*data)
-    v_data = vectorize_data(*data)
+    viz_data(*data[0], data[1])
     regression_model = RegressionModel(2, [3, 3])
     epochs = 1000
-    train_losses, test_losses, predictions, splited_data = run_model(model=regression_model, data=data,
-                                                                     num_of_epochs=epochs)
+    train_losses, test_losses, splited_data = run_model(model=regression_model, data=data, num_of_epochs=epochs)
 
     viz_epochs(num_of_epochs=epochs, other_axis=[train_losses, test_losses], plot_test=True, )
 
-    viz_data(splited_data['x_test'][:, 0], splited_data['x_test'][:, 1], splited_data['y_test'][:,0])
-    viz_data(splited_data['x_test'][:, 0], splited_data['x_test'][:, 1], predictions)
+    viz_preds(data, splited_data['pred'], )
 
 
 def main_overfit():
-    ## overfit part
+    manual_seed(1202)
     data = generate_data()
-    v_data = vectorize_data(*data)
+    viz_data(*data[0], data[1])
+    regression_model = OverfitModel(2, [5, 5, 5])
     epochs = 10000
-    reg_overfit_model = OverfitModel(2, [5, 5, 5])
-    train_losses, test_losses, predictions, splited_data = run_model(reg_overfit_model, data=v_data,
-                                                                     num_of_epochs=epochs)
+    train_losses, test_losses, splited_data = run_model(model=regression_model, data=data, num_of_epochs=epochs)
 
     viz_epochs(num_of_epochs=epochs, other_axis=[train_losses, test_losses], plot_test=True, )
 
+    viz_preds(data, splited_data['pred'], )
+
 
 if __name__ == '__main__':
-    main()
+    main_overfit()
